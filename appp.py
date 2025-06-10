38f21fc15807acefb9cff9179414861d6109ecb5
matheusrochaz-dev <matheusrochasouza08@gmail.com>
2025-06-10 18:17:03 -0300

    d

---
+++ appp.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS 
+import os
+
+
+app = Flask(__name__)
+CORS(app, origins=["https://codax-six.vercel.app/noticias/visita.html?", "http://127.0.0.1:5500"])  
+ARQUIVO_COMENTARIOS = "comentari.txt"
+
+@app.route('/comentariosa', methods=['GET'])
+def obter_comentarios():
+    try:
+        with open(ARQUIVO_COMENTARIOS, "r", encoding="utf-8") as f:
+            comentarios = f.read()
+    except FileNotFoundError:
+        comentarios = "Nenhum comentário ainda."
+    
+    return jsonify({"comentari": comentarios})
+
+@app.route('/comentara', methods=['POST'])
+def comentar():
+    comentario = request.json.get("comentario", "").strip()
+    if comentario:
+        with open(ARQUIVO_COMENTARIOS, "a", encoding="utf-8") as f:
+            f.write(comentario + "\n\n")
+    return jsonify({"message": "Comentário adicionado com sucesso!"}), 201
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
