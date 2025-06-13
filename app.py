0f9bdc57ba43f79c57204657f7dfeef38bbdf39d
matheusrochaz-dev <matheusrochasouza08@gmail.com>
2025-06-13 16:44:45 -0300

    comentariosdiferentes

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ headers = {
     "Content-Type": "application/json"
 }
 
+# tabela comentarios
 @app.route('/comentarios', methods=['GET'])
 def obter_comentarios():
     url = f"{SUPABASE_URL}/rest/v1/comentarios?select=comentario,data&order=data.desc"
@@ -31,6 +32,35 @@ def comentar():
     comentario = request.json.get("comentario", "").strip()
     if comentario:
         url = f"{SUPABASE_URL}/rest/v1/comentarios"
+        payload = {
+            "comentario": comentario
+        }
+        response = requests.post(url, headers=headers, json=payload)
+        if response.ok:
+            return jsonify({"message": "Comentário adicionado com sucesso!"}), 201
+        else:
+            print("STATUS:", response.status_code)
+            print("TEXTO:", response.text)
+            return jsonify({"erro": "Erro ao salvar comentário."}), 500
+    return jsonify({"erro": "Comentário vazio."}), 400
+
+# tabela comentarios 2
+
+@app.route('/comentariosDois', methods=['GET'])
+def obter_comentarios():
+    url = f"{SUPABASE_URL}/rest/v1/comentariosDois?select=comentario,data&order=data.desc"
+    response = requests.get(url, headers=headers)
+    if response.ok:
+        dados = response.json()
+        comentarios = "\n\n".join([c["comentario"] for c in dados])
+        return jsonify({"comentarios": comentarios})
+    return jsonify({"erro": "Erro ao buscar comentários."}), 500
+
+@app.route('/comentarDois', methods=['POST'])
+def comentar():
+    comentario = request.json.get("comentario", "").strip()
+    if comentario:
+        url = f"{SUPABASE_URL}/rest/v1/comentariosDois"
         payload = {
             "comentario": comentario
         }
