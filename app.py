f47890c78d354fac675a3b6b787b076c21b4211f
matheusrochaz-dev <matheusrochasouza08@gmail.com>
2025-06-12 23:09:19 -0300

    j

--- app.py
+++ app.py
@@ -46,3 +46,7 @@ def comentar():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route('/')
+def index():
+    return "API funcionando"
